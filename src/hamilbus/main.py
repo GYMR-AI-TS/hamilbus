8a7e946dd8485c9d58fff6c548e60c7734ab1285
KarlLbs <karl.lubbos@ensea.fr>
2026-04-16 19:47:35 +0200

    Add pytest as a dependency and update main function to return a value

--- src/hamilbus/main.py
+++ src/hamilbus/main.py
@@ -1,5 +1,6 @@
 def main():
     print("Hello from hamilbus!")
+    return "Hello from hamilbus!"
 
 
 if __name__ == "__main__":
---
+++ tests/test_package.py
@@ -0,0 +1,4 @@
+from hamilbus.main import main
+
+def test_package():
+    assert main() == "Hello from hamilbus!"
