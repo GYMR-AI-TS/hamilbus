08505ac854065ba3ff0392596196159ad8bbe2e4
KarlLbs <karl.lubbos@ensea.fr>
2026-04-17 09:52:07 +0200

    Update determine_belonging and merge_stops_by_name methods

--- src/hamilbus/graph_builder.py
+++ src/hamilbus/graph_builder.py
@@ -4,55 +4,64 @@
 from pyproj import Transformer
 from shapely.geometry import Point, LineString
 from collections import defaultdict
-
+from hamilbus.datamodels import Stop, Line
 
 class GraphBuilder():
     """"""
-    def __init__(self):
-        pass
+    def __init__(self, stops:list[Stop], lines:list[Line]):
+        self.stops = stops
+        self.lines = lines
 
-    def project(self, lon, lat):
+    def project(self, lon:float, lat:float):
         '''Project (lon,lat) coordinates in meters'''
         transformer = Transformer.from_crs("EPSG:4326", "EPSG:2154", always_xy=True)
-        return transformer.transform(lon, lat)
+        return transformer.transform(lon, lat) # lon,lat instead of lat,lon by convention (always_xy=True)
 
-    def extract_lines_shapes(self, file):
-        lines = []
-        return lines
+    def determine_belonging(self, stop:Stop, line:Line, threshold=50) -> bool:
+        '''Determine if a stop belongs to a line by checking if it is close enough'''
+        stop_projected = Point(self.project(stop.lon, stop.lat))
+        shape_projected = [self.project(coords[1], coords[0]) for coords in line.shape]
+        linestring = LineString(shape_projected)
+        
+        distance = stop_projected.distance(linestring)
+        if distance < threshold:
+            if line.index not in stop.lines : # We populate stop.lines but line.stops will come after deduping stops
+                stop.lines.append(line.index)
+            return True
+        else : 
+            return False
 
-    def extract_stops(self, file):
-        stops = []
-        return stops
 
-    def merge_stops_by_name(self, stops):
+    def merge_stops_by_name(self, stops:dict[int,Stop]) -> dict[int,Stop]:
         """Merge stops sharing the same name using centroid coordinates."""
         grouped = defaultdict(list)
         for stop in stops:
-            grouped[stop["name"]].append(stop)
-
-        merged_stops = []
+            grouped[stop.name].append(stop) # {"stopName" : [all stops with that name], ...}
+        merged_stops = {}
         for name, group in grouped.items():
-            centroid_lon = sum(stop["lon"] for stop in group) / len(group)
             centroid_lat = sum(stop["lat"] for stop in group) / len(group)
-            merged_stops.append(
-                {
-                    "name": name,
-                    "lon": centroid_lon,
-                    "lat": centroid_lat,
-                }
+            centroid_lon = sum(stop["lon"] for stop in group) / len(group)
+            centroid_lines = []
+            for stop in group:
+                centroid_lines += [line for line in stop.lines if line not in centroid_lines]
+                if stop.type == "parent_station":
+                    idx = stop.index
+            centroid_stop = Stop(
+                index = idx,
+                name = name,
+                type = "centroid",
+                lat = centroid_lat,
+                lon = centroid_lon,
+                lines = centroid_lines, 
+                parent_station_idx = None
             )
-
+            merged_stops[idx] = centroid_stop
         return merged_stops
 
-    def determine_belonging(self, stop, lines, threshold=50):
-        stop_projected = Point(self.project(point))
-        lines = [LineString([self.project(lon, lat) for lon, lat in lines])]
+    def order_stops(self, line):
+        pass
 
-        for line in lines : 
-            distance = stop_projected.distance(line)
-            if distance < threshold:
-                point = point ### TODO : point belongs to line
-
-        return point
+    def build_graph(self, lines):
+        pass
 
 
