deebef68e9c33c496707863a7494fda801d412bf
KarlLbs <karl.lubbos@ensea.fr>
2026-04-29 21:55:19 +0200

    Refactor reader.py, change method names

--- src/hamilbus/reader.py
+++ src/hamilbus/reader.py
@@ -8,10 +8,10 @@ from collections import defaultdict
 from hamilbus.datamodels import Stop, Line
 
 
-def load_stops(path: str | Path) -> list[Stop]:
-    """Load stops and returns them as a list of Stop objects"""
+def load_stops(stops_path: str | Path) -> list[Stop]:
+    """Reads the stops.txt file and returns stops as a list of Stop objects"""
     stops = []
-    with open(path, encoding="utf-8") as f:
+    with open(stops_path, encoding="utf-8") as f:
         stops_file = csv.DictReader(f)
         for row in tqdm(stops_file, desc="Reading stops in stops.txt", unit=" stops"):
             stop = Stop(
@@ -29,7 +29,7 @@ def load_stops(path: str | Path) -> list[Stop]:
 
 
 def load_lines(routes_path: str | Path) -> list[Line]:
-    """Load lines and returns them as a list of Line objects"""
+    """Reads the routes.txt file and returns lines as a list of Line objects"""
     lines = []
     with open(routes_path, encoding="utf-8") as f:
         routes_file = csv.DictReader(f)
@@ -44,21 +44,23 @@ def load_lines(routes_path: str | Path) -> list[Line]:
     return lines
 
 
-def load_lines_trips(trips_path: str | Path) -> dict[str, list[str]]:
+def load_trips_per_line(trips_path: str | Path) -> dict[str, list[str]]:
+    """Reads the trips.txt file and returns a dict of lists of trips ids keyed by lines ids"""
     with open(trips_path, encoding="utf-8") as f:
         trips_file = csv.DictReader(f)
         trips_by_lines = defaultdict(list)
-        for row in tqdm(trips_file, desc="Reading trips in trips.txt", unit=" rows"):
+        for row in tqdm(trips_file, desc="Reading trips in trips.txt", unit=" trips"):
             trips_by_lines[row["route_id"]].append(row["trip_id"])
     return trips_by_lines
 
 
-def load_trips_stops(stop_times_path: str | Path) -> dict[str, list[str]]:
+def load_stops_per_trip(stop_times_path: str | Path) -> dict[str, list[str]]:
+    """Reads the stop_times.txt file and returns a dict of list of stops keyed by trips ids"""
     with open(stop_times_path, encoding="utf-8") as f:
         stop_times_file = csv.DictReader(f)
         stops_by_trips = defaultdict(list)
         for row in tqdm(
-            stop_times_file, desc="Reading trips stops in stop_times.txt", unit=" trips"
+            stop_times_file, desc="Reading trips stops in stop_times.txt", unit=" stops"
         ):
             stops_by_trips[row["trip_id"]].append(row["stop_id"])
     return stops_by_trips
