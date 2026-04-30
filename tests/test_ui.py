4bd6508138c785e47580c91e4d44b63f257030d5
KarlLbs <karl.lubbos@ensea.fr>
2026-04-30 11:43:38 +0200

    Adapt test ui to new data format

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -6,32 +6,45 @@ from hamilbus.web.app import api_stops, api_lines, set_network, set_graph_networ
 
 def test_serializers():
     stops = [
-        Stop(index=0, name="Stop 1", type="parent_station", lat=40.7128, lon=-1.5006),
-        Stop(index=1, name="Stop 2", type="parent_station", lat=40.7129, lon=-1.5007),
-        Stop(index=2, name="Stop 3", type="substation", lat=40.7130, lon=-1.5008),
-        Stop(index=3, name="Stop 4", type="substation", lat=40.7131, lon=-1.5009),
-        Stop(index=4, name="Stop 5", type="substation", lat=52, lon=-12),
+        Stop(id="0", name="Stop 1", type="parent_station", lat=40.7128, lon=-1.5006),
+        Stop(id="1", name="Stop 2", type="parent_station", lat=40.7129, lon=-1.5007),
+        Stop(id="2", name="Stop 3", type="substation", lat=40.7130, lon=-1.5008),
+        Stop(id="3", name="Stop 4", type="substation", lat=40.7131, lon=-1.5009),
     ]
     lines = [
-        Line(index=0, name="Line 1", long_name="Line 1 Long Name", color="red", stops=[stops[0], stops[1]]),
-        Line(index=1, name="Line 2", long_name="Line 2 Long Name", color="blue", stops=[stops[0], stops[2]]),
-        Line(index=2, name="Line 3", long_name="Line 3 Long Name", color="green", stops=[stops[2], stops[3]]),
-        Line(index=3, name="Line 4", long_name="Line 4 Long Name", color="yellow", stops=[stops[1], stops[2], stops[3]]),
+        Line(id="0", name="Line 1", long_name="Line 1 Long Name", color="red"),
+        Line(id="1", name="Line 2", long_name="Line 2 Long Name", color="blue"),
+        Line(id="2", name="Line 3", long_name="Line 3 Long Name", color="green"),
+        Line(id="3", name="Line 4", long_name="Line 4 Long Name", color="yellow"),
     ]
+    trips_by_lines = {
+        "0": ["trip0"],
+        "1": ["trip1"],
+        "2": ["trip2"],
+        "3": ["trip3"],
+    }
+    stops_by_trips = {
+        "trip0": ["0", "1"],
+        "trip1": ["0", "2"],
+        "trip2": ["2", "3"],
+        "trip3": ["1", "2", "3"],
+    }
 
+    graph_builder = GraphBuilder(stops, lines, trips_by_lines, stops_by_trips)
+    graph = graph_builder.build_graph()
+
+    # Network mode
     set_network(stops, lines)
     stops_json_response_network = api_stops()
     lines_json_response_network = api_lines()
 
-    graph_builder = GraphBuilder(stops, lines)
-    graph = graph_builder.build_graph()
-
+    # Graph mode
     set_graph_network(graph)
     stops_json_response_graph = api_stops()
     lines_json_response_graph = api_lines()
 
     assert stops_json_response_network.status_code == stops_json_response_graph.status_code
-    # assert json.loads(stops_json_response_network.body) == json.loads(stops_json_response_graph.body)
+    assert json.loads(stops_json_response_network.body) == json.loads(stops_json_response_graph.body)
 
     assert lines_json_response_network.status_code == lines_json_response_graph.status_code
-    # assert json.loads(lines_json_response_network.body) == json.loads(lines_json_response_graph.body)
+    assert len(json.loads(lines_json_response_network.body)) == len(json.loads(lines_json_response_graph.body))
