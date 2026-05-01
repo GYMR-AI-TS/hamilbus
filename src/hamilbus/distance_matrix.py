1a78e0538a527723084186fadda69a864a2a9745
Polewique <maeva.bachelard.savoie@gmail.com>
2026-05-01 13:55:55 +0200

    refactor: distance matrix based on new team specifications and outputs wanted

--- src/hamilbus/distance_matrix.py
+++ src/hamilbus/distance_matrix.py
@@ -3,13 +3,17 @@
 
 import networkx as nx
 from .datamodels import BusNetworkGraph
-
+import numpy as np
 
 def compute_distance_matrix(
     bus_graph: BusNetworkGraph, strategy: str = "dijkstra"
-) -> dict:
+) -> tuple[np.ndarray, np.ndarray, dict]:
     """Computes the shortest distance and path between all pairs of stops in the graph."""
-    distance_matrix = {}
+
+    stops_id_to_index = {stop_id: idx for idx, stop_id in enumerate(bus_graph.graph.nodes)}
+
+    distance_matrix = np.zeros((len(bus_graph.graph.nodes), len(bus_graph.graph.nodes)))
+    path_matrix = np.empty((len(bus_graph.graph.nodes), len(bus_graph.graph.nodes)), dtype=object)
     strategy_func = {
         "dijkstra": nx.single_source_dijkstra,
         "bellman-ford": nx.single_source_bellman_ford,
@@ -17,12 +21,21 @@ def compute_distance_matrix(
 
     for u in bus_graph.graph.nodes:
         for v in bus_graph.graph.nodes:
+
             if u != v:
                 try:
                     length, path = strategy_func[strategy](
                         bus_graph.graph, u, v, weight="distance"
                     )
-                    distance_matrix[(u, v)] = {"distance": length, "path": path}
+                    path_matrix[stops_id_to_index[u], stops_id_to_index[v]] = path
+                    distance_matrix[stops_id_to_index[u], stops_id_to_index[v]] = length
+
                 except nx.NetworkXNoPath:
-                    distance_matrix[(u, v)] = {"distance": float("inf"), "path": []}
-    return distance_matrix
+                    path_matrix[stops_id_to_index[u], stops_id_to_index[v]] = None  # No path exists
+                    distance_matrix[stops_id_to_index[u], stops_id_to_index[v]] = np.inf  # Infinite distance if no path exists
+
+            else:
+                distance_matrix[stops_id_to_index[u], stops_id_to_index[v]] = 0  # Distance from a node to itself is 0
+                path_matrix[stops_id_to_index[u], stops_id_to_index[v]] = [u]  # Path from a node to itself is just the node
+
+    return path_matrix, distance_matrix, stops_id_to_index
